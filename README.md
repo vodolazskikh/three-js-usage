1cf0a985c8e3c4ec921caee00b46e6f7a5159d00
Александр Водолазских <aleksandr.vodolazskih@Aleksandrs-MacBook-Pro.local>
2022-08-30 17:54:17 +0700

    boilerplate init

---
+++ README.md
@@ -0,0 +1,15 @@
+## Framework agnostic frontend boilerplate.
+
+### Based on webpack 5 and typescript.
+
+### Packages:
+
+1. typescript
+1. webpack
+1. eslint
+1. prettier
+
+### Usage:
+
+1. npm install
+1. npm run start
---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'source-map',
+  optimization: {
+    usedExports: true
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            // disable type checker - we will use it in fork plugin
+            transpileOnly: true
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  plugins: [
+    new ESLintPlugin({
+        extensions: ['.tsx', '.ts', '.js'],
+        exclude: 'node_modules'
+     }),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [{ from: 'src/assets', to: 'assets' }]
+    })
+  ]
+};
